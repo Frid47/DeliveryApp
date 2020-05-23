95c6041a9c5654edbc6a5d0ae8d8ee4274571c32
Frid47 <soukananht@gmail.com>
2020-05-23 23:35:23 +0700

    Create Sign up page

--- lib/screen/sign_up.dart
+++ lib/screen/sign_up.dart
@@ -1,3 +1,4 @@
+import 'package:delivery/utill/input_form.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -6,12 +7,46 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  Column contentContainer() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        InputForm().appLogo(),
+        marginTop(),
+        InputForm().inputName(),
+        marginTop(),
+        InputForm().inputPassword(),
+        marginTop(),
+        InputForm().btnLogIn()
+      ],
+    );
+  }
+
+  SizedBox marginTop() {
+    return SizedBox(
+      height: 20,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Text('Sign up page'),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, Colors.blue],
+              center: Alignment(0, -0.36),
+              radius: 1,
+            ),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: contentContainer(),
+            ),
+          ),
+        ),
       ),
     );
   }
