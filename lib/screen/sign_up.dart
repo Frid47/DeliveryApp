fcf03cc58af648330012894da2f48fe964042c31
Frid47 <soukananht@gmail.com>
2020-05-23 23:57:24 +0700

    Test add file

--- lib/screen/sign_up.dart
+++ lib/screen/sign_up.dart
@@ -1,4 +1,3 @@
-import 'package:delivery/utill/input_form.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -7,46 +6,11 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-  Column contentContainer() {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        InputForm().appLogo(),
-        marginTop(),
-        InputForm().inputName(),
-        marginTop(),
-        InputForm().inputPassword(),
-        marginTop(),
-        InputForm().btnLogIn()
-      ],
-    );
-  }
-
-  SizedBox marginTop() {
-    return SizedBox(
-      height: 20,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: RadialGradient(
-              colors: [Colors.white, Colors.blue],
-              center: Alignment(0, -0.36),
-              radius: 1,
-            ),
-          ),
-          child: Center(
-            child: SingleChildScrollView(
-              child: contentContainer(),
-            ),
-          ),
-        ),
+        child: Text('data'),
       ),
     );
   }
